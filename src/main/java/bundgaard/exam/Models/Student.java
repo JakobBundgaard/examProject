e0cb823e4e0ab88739e64b250bebdfdd7ce9b0e8
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2020-12-21 10:28:32 +0100

    Models added

--- src/main/java/bundgaard/exam/Models/Student.java
+++ src/main/java/bundgaard/exam/Models/Student.java
@@ -12,5 +12,46 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "supervisor_id")
+    private Supervisor supervisor;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Supervisor getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Supervisor supervisor) {
+        this.supervisor = supervisor;
+    }
 }
---
+++ src/main/java/bundgaard/exam/Models/Supervisor.java
@@ -0,0 +1,44 @@
+package bundgaard.exam.Models;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "supervisors")
+public class Supervisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "supervisor")
+    private Set<Student> students;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
